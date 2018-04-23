e256f2a536da83ad40b82abb3d33732b3baf701c
James Ticer <jticer4@icloud.com>
2018-04-23 08:43:48 -0600

    add my arlo function into its own file, and added some code in my body tag that will print out the code I highlight

--- index.php
+++ index.php
@@ -1,18 +1,29 @@
+<?php
+
+// Initialize your autoloader (this example is using composer)
+require 'vendor/autoload.php';
+
+// Instantiate the Highlighter.
+$hl = new Highlight\Highlighter();
+
+// Set the languages you want to detect automatically.
+$hl->setAutodetectLanguages(array('php', 'python', 'perl'));
+
+// Highlight some code.
+$highlightMe = $hl->highlightAuto(file_get_contents('arloFunction.php'));
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 	<head>
 		<title>Snap Composer</title>
+		<link rel="function" type="text/html" href="./arloFunction.php">
 		<meta charset="utf-8"/>
 	</head>
 	<body>
-		<?php
-		function numLivesPerSenator(string $name): int {
-			if($name === "Arlo") {
-				return 9;
-			} else {
-				return 1;
-			}
-		}
-		?>
+		<!-- Print the highlighted code: -->
+		<pre class="hljs <?= $highlightMe->language ?>">
+      <?= $highlightMe->value ?>
 	</body>
 </html>
