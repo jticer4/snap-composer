edde588ff8655214b89a590a5c87b7b605be4ec3
James Ticer <jticer4@icloud.com>
2018-04-23 09:20:55 -0600

    added comments as per my feedback

--- arloFunction.php
+++ arloFunction.php
@@ -1,4 +1,5 @@
 <?php
+// did not properly follow psr4 for my file name
 function numLivesPerSenator(string $name): int {
 	if($name === "Arlo") {
 		return 9;
--- index.php
+++ index.php
@@ -18,12 +18,12 @@ $highlightMe = $hl->highlightAuto(file_get_contents('arloFunction.php'));
 <html lang="en">
 	<head>
 		<title>Snap Composer</title>
-		<link rel="function" type="text/html" href="./arloFunction.php">
+		<link rel="function" type="text/html" href="./arloFunction.php"><!--did not need this -->
 		<meta charset="utf-8"/>
 	</head>
 	<body>
 		<!-- Print the highlighted code: -->
-		<pre class="hljs <?= $highlightMe->language ?>">
-      <?= $highlightMe->value ?>
+		<pre class="hljs <?= $highlightMe->language ?>"><!-- forgot my pre tag -->
+      <?= $highlightMe->value ?><!-- the way I started this was weird, just use php not = -->
 	</body>
 </html>
