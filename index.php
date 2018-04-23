58d1e0c82bcacfc9a02876ae4d97dd6ae9cfe386
James Ticer <jticer4@icloud.com>
2018-04-23 08:13:36 -0600

    added my boiler plate and my number of lives per senator function

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Snap Composer</title>
+		<meta charset="utf-8"/>
+	</head>
+	<body>
+		<?php
+		function numLivesPerSenator(string $name): int {
+			if($name === "Arlo") {
+				return 9;
+			} else {
+				return 1;
+			}
+		}
+		?>
+	</body>
+</html>
